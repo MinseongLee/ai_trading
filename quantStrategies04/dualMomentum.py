0a73101b1f9e572daaae1c1b4244a3969c12b274
MinseongLee <dexlee4567@gmail.com>
2021-09-22 18:16:09 +0900

    dual momentum, relative momentum

--- quantStrategies04/dualMomentum.py
+++ quantStrategies04/dualMomentum.py
@@ -101,3 +101,192 @@ def returns(book, ticker):
     return (round(acc_rtn,4))
 
 returns(book,ticker)
+
+'''
+상대 모멘텀 전략
+relative momentum
+'''
+
+import os
+import glob
+import pandas as pd
+import numpy as np
+import datetime
+
+# file list를 뽑을 때 사용 glob
+files = glob.glob('../contents/*.csv')
+
+# create dataframes
+# Monthly
+month_last_df = pd.DataFrame(columns=['Date', 'CODE','1M_RET'])
+# stocks
+stock_df = pd.DataFrame(columns=['Date', 'CODE', 'Adj Close'])
+
+def data_preprocessing(sample, ticker,base_date):
+    # add ticker
+    sample['CODE'] = ticker
+    sample = sample[sample['Date'] >= base_date][['Date','CODE','Adj Close']].copy()
+    # 기준일자 이후 데이터 사용
+    sample.reset_index(inplace=True, drop=True)
+    sample['STD_YM'] = sample['Date'].map(lambda x : datetime.datetime.strptime(x, '%Y-%m-%d').strftime('%Y-%m'))
+    # return column
+    sample['1M_RET'] = 0.0
+    ym_keys = list(sample['STD_YM'].unique())
+    return sample, ym_keys
+
+# load each csv file
+for csv in files:
+    if os.path.isdir(csv):
+        print('%s <DIR> '%csv)
+    else:
+        folder, name = os.path.split(csv)
+        head, tail = os.path.splitext(name)
+        print('csv = ',csv)
+        read_df = pd.read_csv(csv)
+        # first, preprocessing
+        price_df, ym_keys = data_preprocessing(read_df, head, base_date='2010-01-02')
+        # use the data
+        stock_df = stock_df.append(price_df.loc[:,['Date', 'CODE','Adj Close']],sort=False)
+        # 1개월간 수익률 계산 for 월별 상대 모멘텀 계산
+        for ym in ym_keys:
+            m_ret = price_df.loc[price_df[price_df['STD_YM'] == ym].index[-1], 'Adj Close'] \
+                / price_df.loc[price_df[price_df['STD_YM'] == ym].index[0],'Adj Close']
+            price_df.loc[price_df['STD_YM'] == ym, ['1M_RET']] = m_ret
+            month_last_df = month_last_df.append(price_df.loc[price_df[price_df['STD_YM'] == ym].index[-1],['Date', 'CODE','1M_RET']])
+
+# second, filtering from relative momentum returns
+month_ret_df = month_last_df.pivot('Date', 'CODE', '1M_RET').copy()
+month_ret_df = month_ret_df.rank(axis=1, ascending=False, method='max', pct=True)
+# 상위 40% 안에 드는 종목들만 signal list
+month_ret_df = month_ret_df.where(month_ret_df < 0.4, np.nan)
+month_ret_df.fillna(0, inplace=True)
+month_ret_df[month_ret_df != 0] = 1
+stock_codes = list(stock_df['CODE'].unique())
+
+# third, trading + positioning of signal list
+def create_trade_book(sample, sample_codes):
+    book = pd.DataFrame()
+    book = sample[sample_codes].copy()
+    book['STD_YM'] = book.index.map(lambda x : datetime.datetime.strptime(x, '%Y-%m-%d').strftime('%Y-%m'))
+    # book['trade'] = ''
+    # 종목이 다수이므로, 종목에 따라 포지션과 수익률을 기록한다.
+    for c in sample_codes:
+        book['p '+c] = ''
+        book['r '+c] = ''
+    return book
+
+sig_dict = dict()
+for date in month_ret_df.index:
+    ticker_list = list(month_ret_df.loc[date,month_ret_df.loc[date,:] >= 1.0].index)
+    sig_dict[date] = ticker_list
+stock_c_matrix = stock_df.pivot('Date', 'CODE', 'Adj Close').copy()
+book = create_trade_book(stock_c_matrix, list(stock_df['CODE'].unique()))
+
+# positioning
+for date, values in sig_dict.items():
+    for stock in values:
+        book.loc[date, 'p '+stock] = 'ready ' + stock
+
+# print(book.loc['2011-12-28':'2012-03-01',['AAPL', 'p AAPL', 'r AAPL']])
+
+def tradings(book, s_codes):
+    std_ym = ''
+    buy_phase = False
+    for s in s_codes:
+        print('s_code= ',s)
+        for date in book.index:
+            if book.loc[date,'p '+s] == '' and book.shift(1).loc[date,'p '+s] == 'ready ' + s:
+                std_ym = book.loc[date, 'STD_YM']
+                buy_phase = True
+            if book.loc[date, 'p '+s] == '' and book.loc[date, 'STD_YM'] == std_ym and buy_phase == True:
+                book.loc[date,'p '+s] = 'buy ' + s
+            if book.loc[date,'p '+s] == '':
+                std_ym = None
+                buy_phase = False
+    return book
+# exec trading
+book = tradings(book, stock_codes)
+
+# print(book.loc['2012-01-27':'2012-03-01',['AAPL', 'p AAPL', 'r AAPL']])
+
+def multi_returns(book, tickers):
+    rtn = 1.0
+    buy_dict = {}
+    num = len(tickers)
+    sell_dict = {}
+
+    for date in book.index:
+        for ticker in tickers:
+            p_status = 'p ' + ticker
+            r_status = 'r ' + ticker
+            buy_status = 'buy ' + ticker
+            ready_status = 'ready ' + ticker
+            if book.loc[date, p_status] == buy_status and \
+                book.shift(1).loc[date, p_status] == ready_status and \
+                book.shift(2).loc[date, p_status] == '':
+                # long 진입
+                buy_dict[ticker] = book.loc[date, ticker]
+            elif book.loc[date, p_status] == '' and book.shift(1).loc[date, p_status] == buy_status:
+                # long 청산
+                sell_dict[ticker] = book.loc[date, ticker]
+                # 손익 계산
+                rtn = (sell_dict[ticker] / buy_dict[ticker]) -1
+                book.loc[date, r_status] = rtn
+                # 수익률 계산
+                rtn_percentage = round(rtn * 100,2)
+                print('개별 청산일: ',date,'종목 코드 : ',ticker, 'long 진입가격 : ',buy_dict[ticker],'| long 청산가격 : ',sell_dict[ticker], ' | return: ',rtn_percentage,'%')
+            # zero position, long 청산
+            if book.loc[date, p_status] == '':
+                buy_dict[ticker] = 0.0
+                sell_dict[ticker] = 0.0
+
+    acc_rtn = 1.0
+    for date in book.index:
+        rtn = 0.0
+        count = 0
+        for ticker in tickers:
+            p_status = 'p ' + ticker
+            r_status = 'r ' + ticker
+            buy_status = 'buy ' + ticker
+            if book.loc[date,p_status] == '' and book.shift(1).loc[date, p_status] == buy_status:
+                # 청산 수익 나오므로,
+                count += 1
+                rtn += book.loc[date, r_status]
+        if (rtn != 0.0) & (count != 0):
+            acc_rtn *= (rtn / count) + 1
+            # 창산 수익률
+            sell_return = round((rtn / count),4)
+            print('누적 청산일 : ',date,' 청산 종목수 = ',count,
+                  '창산 수익률 = ',sell_return,' 누적 수익률 = ',round(acc_rtn,4))
+        # 수익률 계산
+        book.loc[date,'acc_rtn'] = acc_rtn
+    print('누적 수익률 : ',round(acc_rtn,4))
+
+
+
+# returns of tickers
+multi_returns(book, stock_codes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
